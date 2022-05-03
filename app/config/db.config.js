cfc9ffa4b77a3f2ce9283dc010324d127ec65bbb
Nish7156 <shedagenishant40@gmail.com>
2022-05-03 12:36:57 +0530

    make it better

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://api_test:DuWFy9lunAMpTr1E@apitest.llbhl.mongodb.net/test"
+};
---
+++ app/models/tutorial.model.js
@@ -0,0 +1,20 @@
+module.exports = mongoose => {
+  var schema = mongoose.Schema(
+    {
+      name: String,
+      email: String,
+      phone:Number,
+      message:String,
+      published: Boolean
+    }
+  );
+
+  schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Tutorial = mongoose.model("tutorial", schema);
+  return Tutorial;
+};
