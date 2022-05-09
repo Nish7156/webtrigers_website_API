bd81a88c9b1865255b84be90514c5261c63d1d70
Nish7156 <shedagenishant40@gmail.com>
2022-05-09 23:23:46 +0530

    hi

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://13.232.127.213"//"https://www.webtriggers.tech"
+  origin: ["http://13.232.127.213","https://www.webtriggers.tech"]
 };
 
 app.use(cors(corsOptions));
